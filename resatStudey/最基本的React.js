eb884c922d92378b8daea1907a7da619c9e3d7f2
mucfpga <2942667790@qq.com>
2019-05-18 17:12:35 +0800

    添加jsx支持

---
+++ resatStudey/最基本的React.js
@@ -0,0 +1,1 @@
+import React, { Component } from 'react';import ReactDOM from 'react-dom'const myh1=React.createElement('h1',{id:'myh1',title:"AAAAAAA"},"WW")ReactDOM.render(myh1, document.getElementById('root'));
